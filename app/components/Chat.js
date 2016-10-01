aad6ee9f0be54d68bdb0704e024e1c03d07c9455
Timo Horstschäfer <mail@timo-horstschaefer.de>
2016-10-01 10:48:39 +0200

    chat

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-// Cryptocat.Me.username = 'timo';
-// Cryptocat.OMEMO.onAddDevice('master', '');
-// setTimeout(function () {
-//   Cryptocat.XMPP.connect(Cryptocat.Me.username,
-//                          'test', function () {
-//                            console.log('connected');
-//                            Cryptocat.OMEMO.sendMessage('bob', 'Hallo');
-//                           // Cryptocat.XMPP.disconnect(false);
-//                          });
-// }, 1000);
+
+import { Form, FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap';
 
 
 
-export default React.createClass({
+const Chat = React.createClass({
+  componentDidMount () {
+    if (Cryptocat.Me.connected === true)
+      return;
+
+    Cryptocat.Me.username = 'timo';
+    Cryptocat.XMPP.connect(Cryptocat.Me.username, 'test', function () {
+                             console.log('connected');
+                            //  Cryptocat.Win.main = {};
+                            //  Cryptocat.Win.main.roster = {};
+                            //  Cryptocat.Win.main.roster.state = {};
+                            //  Cryptocat.Win.main.roster.state.buddies = {bob: true};
+                            //  Cryptocat.Win.main.roster.updateBuddyStatus = (a,b,c) => {}
+                            //  Cryptocat.XMPP.sendBuddyRequest('timoho');
+                            //  Cryptocat.OMEMO.sendMessage('bob', 'Hallo');
+                            // Cryptocat.XMPP.disconnect(false);
+                          }
+    );
+  },
+
+  getInitialState() {
+    return {
+      text: '',
+    };
+  },
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.text);
+
+    var stamp = Date.now();
+		var deviceName = Cryptocat.Me.settings.deviceName;
+    Cryptocat.OMEMO.sendMessage('timoho', {
+      message: this.state.text,
+			internalId: `${deviceName}_${stamp}`
+    });
+    this.setState({ text: '' });
+
+  },
   render() {
     return (
       <div>
         <div>
           <h2>Chat</h2>
+
+              <Form onSubmit={this.handleSubmit} >
+                  <FormGroup>
+                    <FormControl
+                      type="text"
+                      value={this.state.text}
+                      onChange={this.handleChange}
+                      placeholder="Type your message here ..."
+                    />
+                    <Button type="submit">Submit</Button>
+                </FormGroup>
+              </Form>
           <Link to="/">home</Link>
         </div>
       </div>
     );
   }
 });
+
+export default Chat;
