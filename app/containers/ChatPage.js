b23b42f0e4024f164321e19ec562c45d98fa9f20
Timo Horstschäfer <mail@timo-horstschaefer.de>
2016-10-12 19:07:46 +0200

    correct message list on startup

--- app/containers/ChatPage.js
+++ app/containers/ChatPage.js
@@ -6,34 +6,36 @@ import * as ChatActions from '../actions/chat';
 
 
 function mapStateToProps(state) {
-  var otherId = state.chat.activeChat;
-  var otherEmail = state.contacts[otherId].email;
-  var myEmail = state.settings.profile.email;
-
-  var mergeTime = 5 * 60 * 1000;
-
   var messages = [];
-  state.chat.messages[otherId].forEach((msg, i, array) => {
-    if (i > 0 &&
-        msg.fromMe == array[i-1].fromMe &&
-        msg.stamp - array[i-1].stamp < mergeTime) {
-          var oldMsg = messages[messages.length-1];
-          oldMsg.stamp = msg.stamp;
-          oldMsg.contents.push(msg);
-        }
-    else {
-      messages.push({
-        id: msg.id,
-        fromMe: msg.fromMe,
-        username: msg.fromMe ? myEmail : otherEmail,
-        stamp: msg.stamp,
-        contents: [
-          {...msg}
-        ]
-      });
-    }
-  });
-  console.log(messages);
+  var otherId = state.chat.activeChat;
+
+  if (otherId !== '') {
+    var otherEmail = state.contacts[otherId].email;
+    var myEmail = state.settings.profile.email;
+
+    var mergeTime = 5 * 60 * 1000;
+
+    state.chat.messages[otherId].forEach((msg, i, array) => {
+      if (i > 0 &&
+          msg.fromMe == array[i-1].fromMe &&
+          msg.stamp - array[i-1].stamp < mergeTime) {
+            var oldMsg = messages[messages.length-1];
+            oldMsg.stamp = msg.stamp;
+            oldMsg.contents.push(msg);
+          }
+      else {
+        messages.push({
+          id: msg.id,
+          fromMe: msg.fromMe,
+          username: msg.fromMe ? myEmail : otherEmail,
+          stamp: msg.stamp,
+          contents: [
+            {...msg}
+          ]
+        });
+      }
+    });
+  }
 
   return {
     messages,
