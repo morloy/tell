d229d01872b03e3fdf0a39009ae4817653424f5a
Timo Horstschäfer <mail@timo-horstschaefer.de>
2016-12-29 17:48:02 +0100

    Improve connection handling, fix #10

--- app/containers/ConnectionManager.js
+++ app/containers/ConnectionManager.js
@@ -9,19 +9,64 @@ function mapStateToProps({profile}) {
   };
 }
 
+export const NOT_INITIALIZED = 'NOT_INITIALIZED';
+export const CONNECTED = 'CONNECTED';
+export const DISCONNECTED = 'DISCONNECTED';
+export const RECONNECTING = 'RECONNECTING';
+
+const INIT_TIMEOUT = 1;
+const MAX_TIMEOUT = 32;
+
 const ConnectionManager = React.createClass({
   getInitialState() {
     return {
-      online: undefined
+      online: NOT_INITIALIZED,
+      reconnect_timeout: INIT_TIMEOUT
     };
   },
+  setup() {
+    if (window.onbeforeunload !== null) { return; }
+
+    window.onbeforeunload = (e) => {
+      console.log('Shutdown. Disconnecting ...');
+      this.disconnect();
+      Cryptocat.Storage.sync();
+    };
+    window.onoffline = (e) => {
+      console.log('Offline. Disconnecting ...');
+      this.disconnect();
+    };
+    window.ononline  = (e) => {
+      console.log('Online. Reconnecting ...');
+      this.reconnect();
+    };
+    Remote.powerMonitor.on('suspend', () => {
+      console.log('Going to sleep. Disconnecting ...');
+      this.disconnect();
+    });
+    Remote.powerMonitor.on('resume', () => {
+      console.log('Wake up. Reconnecting ...');
+      this.reconnect();
+    });
+    Cryptocat.Win.main.login = { onAuthFailed: () => this.reconnect() };
+  },
   reconnect() {
-    setTimeout(this.connect, 10000);
+    const timeout = this.state.reconnect_timeout;
+    this.setState({
+      online: RECONNECTING,
+      reconnect_timeout: 2*timeout > MAX_TIMEOUT ? MAX_TIMEOUT : 2*timeout
+    });
+    console.log(`Waiting ${timeout} seconds for next reconnect.`);
+    setTimeout(this.connect, timeout*1000);
+  },
+  disconnect() {
+    this.setState({ online: DISCONNECTED });
+    Cryptocat.XMPP.disconnect(false);
   },
   connect() {
     var { username, password } = this.props.profile;
 
-    if (this.state.online === true) {
+    if (this.state.online === CONNECTED) {
       return;
     }
     if (!(username && password)) {
@@ -30,7 +75,7 @@ const ConnectionManager = React.createClass({
 
     Cryptocat.XMPP.connect(username, password, (s) => {
       if (s) {
-        this.setState({ online: true });
+        this.setState({ online: CONNECTED, reconnect_timeout: INIT_TIMEOUT });
 
         client.subscribeToNode(
   				`${username}@${HOSTNAME}`,
@@ -38,32 +83,23 @@ const ConnectionManager = React.createClass({
   			).then((res) => {
           Cryptocat.XMPP.sendDeviceList(Cryptocat.Me.settings.deviceIds);
           Cryptocat.XMPP.sendBundle();
-
-          window.onbeforeunload = (e) => {
-            Cryptocat.XMPP.disconnect(false);
-            Cryptocat.Storage.sync();
-          };
-          window.onoffline = (e) => {
-            Cryptocat.XMPP.disconnect(false);
-            this.setState({ online: false });
-           };
-          window.ononline  = (e) => { this.connect(); };
-          Cryptocat.Win.main.login = { onAuthFailed: () => this.reconnect() };
+          this.setup();
         });
-      } else {
-        this.setState({ online: false });
+      } else if (this.state.online !== DISCONNECTED) {
+        console.log('Connection lost. Reconnecting ...');
         this.reconnect();
       }
     });
   },
   componentWillMount() { this.connect(); },
   componentDidUpdate() {
-    if (this.state.online === undefined) { this.connect(); }
+    if (this.state.online === NOT_INITIALIZED) { this.connect(); }
   },
   render() {
     return (
       <Modal
-        show={this.state.online === false}
+        show={this.state.online !== CONNECTED &&
+              this.state.online !== NOT_INITIALIZED}
         animation={false}
       >
         <Modal.Body>
